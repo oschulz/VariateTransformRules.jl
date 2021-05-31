8fbd53dacd579f467372fb25bed53570d87cb61f
Oliver Schulz <oschulz@mpp.mpg.de>
2021-05-31 21:53:47 +0200

    Add deps

--- src/VariateTransformRules.jl
+++ src/VariateTransformRules.jl
@@ -11,4 +11,8 @@ module VariateTransformRules
 
 include("hello_world.jl")
 
+function __init__()
+    # @require DistributionsAD = "ced4e74d-a319-5a8a-b0ac-84af2272839c" include("distributions_ad.jl")
+end
+
 end # module
